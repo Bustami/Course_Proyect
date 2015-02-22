3e86e3e223a31bdb56f675ef970d9e67a6d76aea
Bustami <ismaelgomezs@gmail.com>
2015-02-22 17:20:58 -0400

    Update run_analysis.R
    uploading

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,4 @@
 ## Read data
-
 # Train
 train<-read.table("./train/X_train.txt")
 trainLa<-read.table("./train/y_train.txt")
@@ -15,34 +14,32 @@ features<-read.table("./features.txt")
 activity<-read.table("./activity_labels.txt")
 
 ## Merge Data
-
 dataMain<-merge(train,test,all=TRUE)
 features<-t(features[,2])
 colnames(dataMain)<-make.names(as.character(features), unique=TRUE)
 dataSub<-merge(trainSub,testSub,all=TRUE)
+colnames(dataSub)<-"Subject"
 dataLa<-rbind(trainLa,testLa)
-dataMain<-cbind(dataMain,dataSub,dataLa)
+colnames(dataLa)<-"Activity_Label"
 
 ## Extract Mean & Std from measurements
-
 names(dataMain)<-make.names(names(dataMain),unique=TRUE)
 dataMean<-select(dataMain,contains("mean"))
 dataStd<-select(dataMain,contains("std"))
-dataSelect<-cbind(dataMean,dataStd)
-
-## Name the activities
-
-dataMain<-mutate(dataMain,Activiy = activity[dataMain[,563],2])
-dataSelect<-cbind(dataSelect,dataMain[,564])
-dataSelect<-cbind(dataSelect,dataSub)
+dataUse<-cbind(dataMean,dataStd,dataSub,dataLa)
 
 ## Mean of each variable for each activity and each subject
+tidy_data<-group_by(dataUse,Subject,Activity_Label)
+dataMean<-summarise_each(tidy_data,funs(mean))
 
-dataMelt<-melt(dataSelect,id=c("dataMain[, 564]","V1"),measure.vars=colNames[1:86])
-MeanActivity<-dcast(xmelt,dataMain[, 564] ~ variable,mean)
-MeanSubject<-dcast(xmelt,V1 ~ variable,mean)
-MeanAll<-merge(MeanActivity,MeanSubject,by.x="dataMain[, 564]",by.y="V1",all=TRUE)
+## Change and Order the Names
+dataMean[,2]<-activity[,2]
+colnames(dataMean)<-gsub("Activity_Label","Activity",colnames(dataMean),fixed = TRUE)
+colnames(dataMean)<-gsub(".","",colnames(dataMean),fixed = TRUE)
+colnames(dataMean)<-gsub("std","Std",colnames(dataMean),fixed = TRUE)
+colnames(dataMean)<-gsub("mean","Mean",colnames(dataMean),fixed = TRUE)
+colnames(dataMean)<-gsub("BodyBody","Body",colnames(dataMean),fixed = TRUE)
 
 ## Output
+write.table(dataMean,file = "dataFinal.txt",row.name=FALSE)
 
-write.table(MeanAll,file = "dataFinal.txt",row.name=FALSE)
